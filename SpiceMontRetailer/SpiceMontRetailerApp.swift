1fa3444b1111541c0ce78f7c2c2c85f7603e33a9
Yudhishthir-reva <yudhishthir@revateam.com>
2026-08-23 17:12:52 +0530

    Initial Commit

---
+++ SpiceMontRetailer/SpiceMontRetailerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiceMontRetailerApp.swift
+//  SpiceMontRetailer
+//
+//  Created by Reva on 23/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SpiceMontRetailerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
